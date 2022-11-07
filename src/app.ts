47d1b45c6049339d5fa0cebaac5e2568d894370a
Elvin Skaria <encorereloded@gmail.com>
2022-11-07 16:02:48 +0530

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,17 @@ import logger from "morgan";
 import bodyParser from 'body-parser'
 
 import routes from "./routes";
-import { open } from "./database/index"
+// import { open } from "./database/index"
 
 const app = express();
-const cors = require("cors");
 app.disable("x-powered-by");
 
-app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(logger("dev", { skip: () => app.get("env") === "test" }));
 app.use(routes);
 
-open()
+// open()
 
 export default app;
